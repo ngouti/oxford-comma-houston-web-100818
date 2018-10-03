b0c3b4097a633111e721d51a6987c0fb4d058dc4
Emma Gouti <goutiemma@gmail.com>
2018-10-03 06:45:27 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+if array.length == 1 
+  array.map { |i| i.to_s }.join(" ");
+elsif array.length == 2 
+  array.map { |i| i.to_s }.join(" and ")
+elsif array.length == 3
+  array[0..-2].join(", ") << ", and #{array.last}"
+else 
+  array[0...-1].join(", ") << ", and #{array.last}"
 end
+end
+
